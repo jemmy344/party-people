2f5e690d6278cef3e4b1b5e34f6c7685b3f71282
Liam <liamjwatson@aol.com>
2023-03-18 12:39:20 +0000

    filters the ticketmaster api response to a map

--- frontend/src/page/LandingPage.js
+++ frontend/src/page/LandingPage.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Populardestinations from '../components/PopularDesination'
 import SpecialOffers from '../components/SpecialOffers'
 import { Search } from '../components/Search'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
+
 
 
 const LandingPage = () => {
   return (
     <div>
+      <Header/>
       <div>
         <Search />
       </div>
