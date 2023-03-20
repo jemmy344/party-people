e94143edd628280b2eb86c6ea57f6142bf1945f2
Jonatan Kwiatkowski <joniqs91@gmail.com>
2023-03-20 16:47:00 +0000

    Added animation when fetching data from ticketmaster api.

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import axios from "axios";
 import { EventCard } from "../EventCard";
 import { Container, Col, Row } from "react-bootstrap";
+import './styles.css';
 
 export const Search = () => {
   const [input, setInput] = useState("");
   const [eventMap, setEventMap] = useState(new Map());
+  const [loading, setLoading] = useState(false);
 
   const getEventData = () => {
+    setLoading(true); // Set loading to true before making the API request
+
     const options = {
       method: "GET",
       url: `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${input}&apikey=kaC44nEKbqApNIGjNUt8hulGkGIVtoPN`,
@@ -35,9 +39,11 @@ export const Search = () => {
           });
         });
         setEventMap(newEventMap);
+        setLoading(false); // Set loading to false after the API request completes
       })
       .catch(function (error) {
         console.error(error);
+        setLoading(false); // Set loading to false if there is an error
       });
   };
 
@@ -50,11 +56,17 @@ export const Search = () => {
     <>
       <input type="text" value={input} onChange={handleChange} />
       <button onClick={getEventData}>Search</button>
+      {/* Show the loading animation if loading is true */}
+      {loading && 
+        <div class="overlay">
+          <div class="lds-facebook"><div></div><div></div><div></div></div>
+        </div>
+      } 
       <Container>
         <Row xs={1} md={2} lg={4} xl={4} className="g-4">
           {[...eventMap.values()].map((event) => (
-            <Col>
-              <EventCard event={event} artistName={input}/>
+            <Col key={event.id}> {/* Add a unique key prop to the list of EventCards */}
+              <EventCard event={event} artistName={input} />
             </Col>
           ))}
         </Row>
