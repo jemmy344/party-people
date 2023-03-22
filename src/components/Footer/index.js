1ad11e97e9cfcb52d7bc1fa68e29f842e3bde8cb
jemimah <jemimah@wirebox.co.uk>
2023-03-22 22:19:20 +0000

    eslinting

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import "./styles.css";
